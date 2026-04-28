278995147c913573da7faa20ec435ac1a8352974
jaredserre <75283203+jaredserre@users.noreply.github.com>
2026-04-28 15:31:02 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Tax Bill Tracker</h1>
+      <p>If you can see this, your app is working.</p>
+    </div>
+  );
+}
